c4e551048a506933660b5d6244b3e677def0e8d3
Chase <chase@myproplatform.com>
2024-08-01 21:58:55 -0400

    initial commit

---
+++ scripts.js
@@ -0,0 +1,79 @@
+// Function to get query parameters
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+const apiKey = getQueryParam('apiKey');
+
+if (!apiKey) {
+    alert('API Key is required');
+    throw new Error('API Key is required');
+}
+
+mapboxgl.accessToken = apiKey;
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/satellite-v9',
+    center: [-74.5, 40],
+    zoom: 9
+});
+
+const draw = new MapboxDraw({
+    displayControlsDefault: false,
+    controls: {
+        polygon: true,
+        trash: true
+    }
+});
+map.addControl(draw);
+
+const geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl,
+    marker: false,
+    placeholder: 'Enter address'
+});
+
+document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+
+geocoder.on('result', function(e) {
+    map.flyTo({
+        center: e.result.center,
+        zoom: 18
+    });
+    document.getElementById('search-container').classList.add('minimized');
+    document.getElementById('instructions').classList.remove('hidden');
+});
+
+map.on('draw.create', updateArea);
+map.on('draw.delete', updateArea);
+map.on('draw.update', updateArea);
+
+function updateArea(e) {
+    const data = draw.getAll();
+    if (data.features.length > 0) {
+        const area = turf.area(data);
+        const rounded_area = Math.round(area * 100) / 100;
+        document.getElementById('calculated-area').innerHTML = `<p>Roof area: ${rounded_area} square meters</p>`;
+        document.getElementById('info-container').classList.remove('hidden');
+        document.getElementById('instructions').classList.add('hidden');
+    } else {
+        document.getElementById('calculated-area').innerHTML = '';
+        document.getElementById('info-container').classList.add('hidden');
+        document.getElementById('instructions').classList.remove('hidden');
+    }
+}
+
+document.getElementById('reset-button').addEventListener('click', function() {
+    draw.deleteAll();
+    document.getElementById('search-container').classList.remove('minimized');
+    document.getElementById('info-container').classList.add('hidden');
+    document.getElementById('instructions').classList.add('hidden');
+    map.flyTo({
+        center: [-74.5, 40],
+        zoom: 9
+    });
+});
+
